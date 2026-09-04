59359459146092f438f945e38815bc1e857eab98
Leon Tvedten <leotve07@gvgs.no>
2026-09-04 13:22:53 +0200

    Fullført oppgave python_3_handlekurv(lagt variabler og regnet pris for det som skulle kjøpe)

---
+++ python_lekse/oppgaver/python_3_handlekurv.py
@@ -0,0 +1,11 @@
+energidrikke = 25
+baguetter = 45
+
+num_energidrikke = 2
+num_baguetter = 3
+
+tot_energidrikke = energidrikke * num_energidrikke
+tot_baguetter = baguetter * num_baguetter
+
+print(f"Pris energidrikker: {tot_energidrikke}\nPris på baguetter: {tot_baguetter}\n \
+      Total: {tot_energidrikke * tot_baguetter}")
