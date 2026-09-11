6a10c6bf9e0e0fdf9abd3f52e10d735fa2514af1
Leon Tvedten <leotve07@gvgs.no>
2026-09-11 11:19:04 +0200

    la til pil opp og pil ned for kursen

--- python_lekse/mine_prosjekter/fake_aksjemarked.py
+++ python_lekse/mine_prosjekter/fake_aksjemarked.py
@@ -12,11 +12,11 @@ for x in range(100):
     kurs.append(start_kurs)
     if len(kurs) >= 2:
         if kurs[-1] > kurs[-2]:
-          print(f"Kurs: [light_green]{kurs[-1]:.2f}[/light_green] kr")
+          print(f"Kurs: [light_green]↑ {kurs[-1]:.2f}[/light_green] kr")
         else: 
-            print(f"Kurs: [red]{kurs[-1]:.2f}[/red] kr")
+            print(f"Kurs: [red]↓ {kurs[-1]:.2f}[/red] kr")
        
-        time.sleep(1)
+        time.sleep(5)
 
 
 
