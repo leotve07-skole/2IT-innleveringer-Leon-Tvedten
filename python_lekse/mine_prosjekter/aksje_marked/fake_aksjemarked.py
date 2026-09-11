be6601c18638a4ca58c51ffc45a7bdbc72bd651d
Leon Tvedten <leotve07@gvgs.no>
2026-09-11 14:51:16 +0200

    fikset logisk feil i matematikken i fil fake_aksjemarked.py på linje 24

--- python_lekse/mine_prosjekter/aksje_marked/fake_aksjemarked.py
+++ python_lekse/mine_prosjekter/aksje_marked/fake_aksjemarked.py
@@ -12,23 +12,23 @@ for x in range(100):
         kurs_print = f"Kurs: [light_green]↑ {db["kurs"][-1]:.2f}[/light_green] kr" if db["kurs"][-1] > db["kurs"][-2] else f"Kurs: [red]↓ {db["kurs"][-1]:.2f}[/red] kr "
         print(kurs_print)
         time.sleep(1)
-        svar = input("Hvis du vil kjøpe aksjer skriv <kjøp aksjer>")
+        print("Hvis du vil kjøpe aksjer skriv <kjøp aksjer>")
 
-        if svar == "kjøp aksjer":
+        if input() == "kjøp aksjer":
             print(f"Hvor mange aksjer vil du kjøpe?\n{kurs_print}\
                   \ndu har [light_green]{db["penger"]}[/light_green] kr på kontoen")
             kjøp_aksjer = int(input())
             if (db["penger"] - (kjøp_aksjer * db["kurs"][-1])) >= 0:
                 db["aksje_beholdning"] += kjøp_aksjer
                 db["penger"] -= (kjøp_aksjer * db["kurs"][-1])
-                db["aksje_beholdning"] = (db["aksje_beholdning"] * db["kurs"][-1])
+                db["aksje_beholdning"] = kjøp_aksjer
                 print(f"Du har kjøpt [light_green]{kjøp_aksjer}[/light_green] aksjer for [red]{(kjøp_aksjer * db["kurs"][-1]):.2f}[/red] kr. \
                 \nDin aksje beholdning er nå verdt [light_green]{db["aksje_beholdning"]:.2f}[/light_green] \
                 \nDu har [red]{db["penger"]:.2f}[/red] kr igjen på kontoen")
                 with open("data.json", "w") as fil:
                     json.dump(db, fil)
             else:
-                print(f"Du har ikke penger på konto til å kjøpe [red]{kjøp_aksjer}[/red] aksjer")
+                print(f"Du har ikke penger nok på konto til å kjøpe [red]{kjøp_aksjer}[/red] aksjer")
         
 
     
