b85af2f80b32657496063192eb4cb2850ae542a4
Leon Tvedten <leotve07@gvgs.no>
2026-09-12 17:41:44 +0200

    In process of switching code to use curses library instead of normal python print

--- python_lekse/mine_prosjekter/aksje_marked/fake_aksjemarked.py
+++ python_lekse/mine_prosjekter/aksje_marked/fake_aksjemarked.py
@@ -3,49 +3,60 @@ import time
 import json
 import threading
 import queue
-from rich import print 
+import curses
+from rich import print as rprint
 from data import db
 
-kommandoer = queue.Queue()
+def aksje_spill(stdscr):
+    stdscr.nodelay(True)
+    max_y, max_x = stdscr.getmaxyx()
+    graf_x = 0
+    graf_y = max_y // 2
+    kommandoer = queue.Queue()
 
-def input_loop():
-    while True:
-        kommando = input()
-        kommandoer.put(kommando.lower())
-      
-threading.Thread(target=input_loop, daemon=True).start()
+    def input_loop():
+        while True:
+            kommando = stdscr.getch()
+            kommandoer.put(kommando)
+        
+    threading.Thread(target=input_loop, daemon=True).start()
 
-for x in range(1000):     
-    endring = random.uniform(-5, 5)
-    db["start_kurs"] += endring
-    db["kurs"].append(db["start_kurs"])
-    if len(db["kurs"]) >= 2:
-        kurs_print = f"Kurs: [light_green]↑ {db["kurs"][-1]:.2f}[/light_green] kr" if db["kurs"][-1] > db["kurs"][-2] else f"Kurs: [red]↓ {db["kurs"][-1]:.2f}[/red] kr "
-        print(kurs_print)
-        time.sleep(1)
-        print(
-            "Hvis du vil kjøpe aksjer skriv <b>\n"
-            "Hvis du vil selge aksjer skriv <s>"
-            )
+    modus = "kurs"
+    for x in range(1000):
+        endring = random.uniform(-5, 5)
+        db["start_kurs"] += endring
+        db["kurs"].append(db["start_kurs"])
+        if modus == "kurs":     
+            if len(db["kurs"]) >= 2:
+                kurs_print = f"Kurs: [light_green]↑ {db["kurs"][-1]:.2f}[/light_green] kr" if db["kurs"][-1] > db["kurs"][-2] else f"Kurs: [red]↓ {db["kurs"][-1]:.2f}[/red] kr "
+                stdscr.addstr(2,2, kurs_print)
+                time.sleep(1)
+                print(
+                    "Hvis du vil kjøpe aksjer skriv <b>\n"
+                    "Hvis du vil selge aksjer skriv <s>\n"
+                    "Skriv <g> hvis du vil se grafisk aksje kursen"
+                    )
+        kommando = None
         if not kommandoer.empty():
             kommando = kommandoer.get()
-            if kommando == "b":
-                print(f"Hvor mange aksjer vil du kjøpe?\n{kurs_print}\
+            if kommando == ord("b"):
+                rprint(f"Hvor mange aksjer vil du kjøpe?\n{kurs_print}\
                     \ndu har [light_green]{db["penger"]:.2f}[/light_green] kr på kontoen")
                 kjøp_aksjer = int(kommandoer.get())
                 if (db["penger"] - (kjøp_aksjer * db["kurs"][-1])) >= 0:
                     db["aksje_beholdning"] += kjøp_aksjer
                     db["penger"] -= (kjøp_aksjer * db["kurs"][-1])
-                    db["aksje_beholdning"] += kjøp_aksjer
-                    print(f"Du har kjøpt [light_green]{kjøp_aksjer}[/light_green] aksjer for [red]{(kjøp_aksjer * db["kurs"][-1]):.2f}[/red] kr. \
+                    rprint(f"Du har kjøpt [light_green]{kjøp_aksjer}[/light_green] aksjer for [red]{(kjøp_aksjer * db["kurs"][-1]):.2f}[/red] kr. \
                     \nDin aksje beholdning er nå verdt [light_green]{(db["aksje_beholdning"] * db["kurs"][-1]):.2f}[/light_green] \
-                    \nDu har [red]{db["penger"]:.2f}[/red] kr igjen på kontoen")
+                    \nDu har [red]{db["penger"]:.2f}[/red] kr igjen på kontoen.\
+                    \nDu har nå {db["aksje_beholdning"]} aksjer"
+                    )
                     with open("data.json", "w") as fil:
                         json.dump(db, fil)
                 else:
-                    print(f"Du har ikke penger nok på konto til å kjøpe [red]{kjøp_aksjer}[/red] aksjer")
-            if kommando == "s":
-                print(
+                    rprint(f"Du har ikke penger nok på konto til å kjøpe [red]{kjøp_aksjer}[/red] aksjer")
+            if kommando == ord("s"):
+                rprint(
                     f"Du har {db["aksje_beholdning"]} aksjer.\n"
                     f"Aksjeverdien tilsvarer [light_green]{(db["aksje_beholdning"] * db["kurs"][-1]):.2f}"
                     )
@@ -54,7 +65,7 @@ for x in range(1000):
                 if (db["aksje_beholdning"] - selg_aksjer) >= 0:
                     db["aksje_beholdning"] -= selg_aksjer
                     db["penger"] += (selg_aksjer * db["kurs"][-1])
-                    print(
+                    rprint(
                         f"Du solgte [light_green]{selg_aksjer}[/light_green] aksjer for [light_green]{(selg_aksjer * db["kurs"][-1]):.2f}[/light_green] kr\n"
                         f"Du har nå [light_green]{db["penger"]:.2f}[/light_green] kr på kontoen"
                         )
@@ -62,3 +73,17 @@ for x in range(1000):
                         json.dump(db, fil)
                 else:
                     print(f"Du har ikke nok antall aksjer til å selge {selg_aksjer} aksjer")
+        if kommando == ord("g"):
+            modus = "graf"
+        if modus == "graf":
+            stdscr.addch(graf_y, graf_x, "*")
+            graf_x += 1
+            if db["kurs"][-1] > db["kurs"][-2]:
+               graf_y -= 1
+            else:
+                graf_y += 1
+        
+        stdscr.refresh()
+
+if __name__ == "__main__":
+    curses.wrapper(aksje_spill)
