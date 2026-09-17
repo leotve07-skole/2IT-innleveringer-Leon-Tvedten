c9b80ee5ced4327cbed74270d3f3a000d1ae1981
Leon Tvedten <leotve07@gvgs.no>
2026-09-17 18:14:19 +0200

    Lagt til alle tall bilder i pixel_art (business-simulator

---
+++ python_lekse/undervisning/Live_koding/terninglek.py
@@ -0,0 +1,21 @@
+terning_kast = []
+t1 = 4
+t2 = 4
+t3 = 5
+t4 = 4
+t5 = 2
+print(((t1 // t2) // t3) * t4 * t5)
+t1 = 1
+t2 = 5
+t3 = 6
+t4 = 5
+t5 = 4
+print(t3 + t5 - t4 - t2 + t1)
+
+t1 = 1
+t2 = 4
+t3 = 4
+t4 = 5
+t5 = 5
+print(t5 * t3 / t2 // t4 + t1)
+
